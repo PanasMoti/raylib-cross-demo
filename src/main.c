881dc6ee634b9a0f13753273b7e06b793b18368c
PanasMoti <eliasyhoshen@gmail.com>
2026-02-26 16:19:14 +0200

    changed structre of main

--- src/main.c
+++ src/main.c
@@ -3,26 +3,41 @@
 #include <raylib.h>
 #include <ini.h>
 #include "load_cfg.h"
+const char* config_path = "assets/cfg.ini";
+config cfg = { 0 };
 
-int main(void) {
-    const char* config_path = "assets/cfg.ini";
-    config cfg = { 0 };
+void preload() {
     if(ini_parse(config_path, handler, &cfg) < 0) {
         fprintf(stderr, "FUCK\n");
     }
-    printf("(%d %d %s)\n", cfg.window.width, cfg.window.height, cfg.window.title);
+    //printf("(%d %d %s)\n", cfg.window.width, cfg.window.height, cfg.window.title);
+}
+void load() {
+    // load assets
+}
+void init() {
     const int width = cfg.window.width;
     const int height = cfg.window.height;
     const char* const title = cfg.window.title;
     InitWindow(width, height, title);
     SetTargetFPS(60);
+}
+void quit() {
+    CloseWindow();
+    free_config(&cfg);
+}
+
+
+int main(void) {
+    preload();
+    load();
+    init();
     while(!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawText("Hello World", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
-    CloseWindow();
-    free_config(&cfg);
+    quit();
     return 0;
 }
